b9b6487bb7a90bf44094191c30a129c491876687
Mari Luz Jimenez <mariluzj799@gmail.com>
2024-07-17 10:29:11 -0500

    ejercicio uno

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,20 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+def encontrar_menor_mayor(lista_numeros):
+    if not lista_numeros:
+        return {}  
+    
+    num_menor = min(lista_numeros)
+    num_mayor = max(lista_numeros)
+    
+    resultado = {
+        "menor": num_menor,
+        "mayor": num_mayor
+    }
+    return resultado
+
+
+numeros = [2, 7, 20, 1, 5]
+resultado = encontrar_menor_mayor(numeros)
+print(resultado)
