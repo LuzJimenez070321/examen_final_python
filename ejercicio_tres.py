d89d728d82204771dfd871f5b71999aa4ea7ce43
Mari Luz Jimenez <mariluzj799@gmail.com>
2024-07-17 09:42:54 -0500

    ejercicio tres

--- ejercicio_tres.py
+++ ejercicio_tres.py
@@ -4,3 +4,16 @@ ejm:
 entrada: eucalipto
 salida: {e:1,u:1,a:1,i:1,o:1}
 """
+def contar_vocales(texto):
+    vocales = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+    
+    for letra in texto.lower():
+        if letra in vocales:
+            vocales[letra] += 1
+            
+    return vocales
+
+# Ejemplo de uso
+texto_ejemplo = "Hola, este es un ejemplo de texto con vocales."
+resultado = contar_vocales(texto_ejemplo)
+print(resultado)
