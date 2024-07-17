2939873986956260997fd4edbb6d01cf3c78bb33
Mari Luz Jimenez <mariluzj799@gmail.com>
2024-07-17 09:38:46 -0500

    actualize el ejercicio dos

--- ejercicio_dos.py
+++ ejercicio_dos.py
@@ -18,9 +18,3 @@ def filtrar_numeros_primos(lista_numeros):
 lista_numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 numeros_primos = filtrar_numeros_primos(lista_numeros)
 print(numeros_primos)
-
-##filtrar_numeros_numeros_primos toma como parametro lista numeros 
-## se cre una lista vacia de numeros primos 
-## cada numero "num" es una lista de numeros 
-## para cada "num" se verefica que sea mayor de 1 
-## brak se utiliza para determinar un blucle cuando se cumpla cierta condicion 
